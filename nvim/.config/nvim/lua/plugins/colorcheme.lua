359dad3b05afa79b157ebb91d23963f91ee01353
Rajdeep Malakar <raj@isworking.dev>
2026-05-27 18:17:39 +0530

    moved from AstroNvim to LazyVim
    Signed-off-by: Rajdeep Malakar <raj@isworking.dev>

---
+++ nvim/.config/nvim/lua/plugins/colorcheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+
+    opts = {
+      flavour = "frappe",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
